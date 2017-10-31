34a564650d5eaf7e5d2be3d427f2cda8ea59f2cd
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-10-31 21:50:26 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var WelcomeMessage = prompt("Welcome to Rock, Paper, Scissors. The first player to reach 5 points will win the game. Press any key to start");
+
+var choice = prompt("please choose from the following, press r for rock, p for paper and s for scissors");
+
+var AI = Math.floor(Math.random())+1
+
+for (var i = 1; i<10; i++) {
+  function computer() {
+    switch (choice) {
+      case "r":
+      
+
+        break;
+      default:
+
+    }
+  }
+}
+
+
+
+
+
+
+
+//   if (AI <= 2) {
+//   var answer = prompt("The computer chose rock, you lost")
+//   return choice
+// } else if (AI <= 4) {
+//   var answer = prompt("The computer chose paper");
+// }
+// }
+
+
+
+// if (choice === "r")
+// function Rock() {
+//   var AI = Math.floor(Math.random() * 6) + 1;
+//   if (AI <= 2)
+//   return prompt("The computer chose rock, you drew");
+// } else if (AI <=4) {
+//   return prompt("The computer chose ppaper ")
+// }
+// }
