49611f1dba6f097bf8c6fb4805fadeda77e4b168
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-01 11:00:48 +0000

    get input section works

---
+++ solution/main.js
@@ -0,0 +1,23 @@
+//get input
+
+function getUserInput() {
+  return prompt("Please choose either 'rock', 'paper or 'scissors.");
+}
+
+var test = getUserInput();
+console.log(test);
+
+
+
+
+
+
+//get the player move
+
+//random player
+
+//get computer move
+
+//function will need to play to 5
+
+//Check for a winner
